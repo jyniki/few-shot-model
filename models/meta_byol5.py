cc40c8e0991430e1b9e57c520cb08b3fd0a5bba4
niki <30403012+jyniki@users.noreply.github.com>
2020-10-23 10:36:17 +0800

    .

---
+++ models/meta_byol5.py
@@ -0,0 +1,196 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import models
+from .models import register
+import copy
+import random
+from functools import wraps
+
+import utils
+
+# exponential moving average
+class EMA():
+    def __init__(self, beta):
+        super().__init__()
+        self.beta = beta
+
+    def update_average(self, old, new):
+        if old is None:
+            return new
+        return old * self.beta + (1 - self.beta) * new
+
+
+def update_moving_average(ema_updater, ma_model, current_model):
+    for i, (current_params, ma_params) in enumerate(zip(current_model.parameters(), ma_model.parameters())):
+        old_weight, up_weight = ma_params.data, current_params.data
+        ma_params.data = ema_updater.update_average(old_weight, up_weight)
+
+
+def singleton(cache_key):
+    def inner_fn(fn):
+        @wraps(fn)
+        def wrapper(self, *args, **kwargs):
+            instance = getattr(self, cache_key)
+            if instance is not None:
+                return instance
+
+            instance = fn(self, *args, **kwargs)
+            setattr(self, cache_key, instance)
+            return instance
+        return wrapper
+    return inner_fn
+
+
+class MLP(nn.Module):
+    def __init__(self, dim, projection_size, hidden_size = 4096):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_size),
+            nn.BatchNorm1d(hidden_size),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_size, projection_size)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+@register('meta-byol5')
+class MetaByol5(nn.Module):
+
+    def __init__(self, encoder, encoder_args={}, method='cos',
+                 temp=10., temp_learnable=True):
+        super().__init__()
+        self.method = method
+        if temp_learnable:
+            self.temp = nn.Parameter(torch.tensor(temp))
+        else:
+            self.temp = temp
+        self.online_encoder = models.make(encoder, **encoder_args)
+        self.online_projector = MLP(self.online_encoder.out_dim, 256, 4096).cuda()
+        self.online_encoder_all = nn.Sequential(self.online_encoder, self.online_projector)
+
+        self.target_encoder_all = None
+
+        self.target_ema_updater = EMA(0.99)
+
+        self.online_predictor = MLP(256, 256, 4096).cuda()
+
+        # send a mock image tensor to instantiate singleton parameters
+        self.forward(torch.randn(1, 5, 1, 3, 84, 84).cuda(), torch.randn(1, 75, 3, 84, 84).cuda(),
+                     torch.randn(1, 5, 1, 3, 84, 84).cuda(), torch.randn(1, 75, 3, 84, 84).cuda(),
+                     torch.randn(1, 5, 1, 3, 84, 84).cuda(), torch.randn(1, 75, 3, 84, 84).cuda())
+
+    @singleton('target_encoder_all')
+    def _get_target_encoder(self):
+        target_encoder_all = copy.deepcopy(self.online_encoder_all)
+        return target_encoder_all
+
+    def reset_moving_average(self):
+        del self.target_encoder_all
+        self.target_encoder_all = None
+
+    def update_moving_average(self):
+        assert self.target_encoder_all is not None, 'target encoder has not been created yet'
+        update_moving_average(self.target_ema_updater, self.target_encoder_all, self.online_encoder_all)
+
+    def forward(self, x_shot_ori, x_query_ori, x_shot_one, x_query_one, x_shot_two, x_query_two):
+
+        shot_shape = x_shot_one.shape[:-3]
+        query_shape = x_query_one.shape[:-3]
+        img_shape = x_shot_one.shape[-3:]
+        x_shot_one = x_shot_one.view(-1, *img_shape)
+        x_query_one = x_query_one.view(-1, *img_shape)
+        x_shot_two = x_shot_one.view(-1, *img_shape)
+        x_query_two = x_query_two.view(-1, *img_shape)
+
+        image_one = torch.cat([x_shot_one, x_query_one], dim=0)
+        image_two = torch.cat([x_shot_two, x_query_two], dim=0)
+
+        online_feat_one = self.online_encoder(image_one)
+        online_feat_two = self.online_encoder(image_two)
+
+        online_proj_one = self.online_projector(online_feat_one)
+        online_proj_two = self.online_projector(online_feat_two)
+
+        online_pred_one = self.online_predictor(online_proj_one)
+        online_pred_two = self.online_predictor(online_proj_two)
+
+        with torch.no_grad():
+            target_encoder_all = self._get_target_encoder()
+            target_proj_one = target_encoder_all(image_one)
+            target_proj_two = target_encoder_all(image_two)
+
+        loss_one = loss_fn(online_pred_one, target_proj_two.detach())
+        loss_two = loss_fn(online_pred_two, target_proj_one.detach())
+
+        loss_byol = loss_one + loss_two
+
+        shot_shape = x_shot_ori.shape[:-3]
+        query_shape = x_query_ori.shape[:-3]
+        img_shape = x_shot_ori.shape[-3:]
+
+        x_shot = x_shot_ori.view(-1, *img_shape)
+        x_query = x_query_ori.view(-1, *img_shape)
+        x_tot = self.online_encoder(torch.cat([x_shot, x_query], dim=0))
+        x_shot, x_query = x_tot[:len(x_shot)], x_tot[-len(x_query):]
+        x_shot = x_shot.view(*shot_shape, -1)
+        x_query = x_query.view(*query_shape, -1)
+
+        # x_shot_one, x_query_one = online_feat_one[:len(x_shot_one)], online_feat_one[-len(x_query_one):]
+        # x_shot_one = x_shot_one.view(*shot_shape, -1)
+        # x_query_one = x_query_one.view(*query_shape, -1)
+        #
+        # x_shot_two, x_query_two = online_feat_two[:len(x_shot_two)], online_feat_two[-len(x_query_two):]
+        # x_shot_two = x_shot_two.view(*shot_shape, -1)
+        # x_query_two = x_query_two.view(*query_shape, -1)
+        #
+        # x_shot = torch.cat([x_shot_one, x_shot_two], dim=2)
+        # x_query = torch.cat([x_query_one, x_query_two], dim=1)
+
+        if self.method == 'cos':
+            x_shot = x_shot.mean(dim=-2)
+            x_shot = F.normalize(x_shot, dim=-1)
+            x_query = F.normalize(x_query, dim=-1)
+            metric = 'dot'
+        elif self.method == 'sqr':
+            x_shot = x_shot.mean(dim=-2)
+            metric = 'sqr'
+
+        logits = utils.compute_logits(
+            x_query, x_shot, metric=metric, temp=self.temp)
+
+        return loss_byol, logits
+
+    def test(self, x_shot, x_query):
+        shot_shape = x_shot.shape[:-3]
+        query_shape = x_query.shape[:-3]
+        img_shape = x_shot.shape[-3:]
+
+        x_shot = x_shot.view(-1, *img_shape)
+        x_query = x_query.view(-1, *img_shape)
+        x_tot = self.online_encoder(torch.cat([x_shot, x_query], dim=0))
+        x_shot, x_query = x_tot[:len(x_shot)], x_tot[-len(x_query):]
+        x_shot = x_shot.view(*shot_shape, -1)
+        x_query = x_query.view(*query_shape, -1)
+
+        if self.method == 'cos':
+            x_shot = x_shot.mean(dim=-2)
+            x_shot = F.normalize(x_shot, dim=-1)
+            x_query = F.normalize(x_query, dim=-1)
+            metric = 'dot'
+        elif self.method == 'sqr':
+            x_shot = x_shot.mean(dim=-2)
+            metric = 'sqr'
+
+        logits = utils.compute_logits(
+            x_query, x_shot, metric=metric, temp=self.temp)
+        return logits
+
+
+def loss_fn(x, y):
+    x = F.normalize(x, dim=-1, p=2)
+    y = F.normalize(y, dim=-1, p=2)
+    return 2 - 2 * (x * y).sum(dim=-1)
---
+++ train_meta_byol5.py
@@ -0,0 +1,251 @@
+import argparse
+import os
+import yaml
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from tqdm import tqdm
+from tensorboardX import SummaryWriter
+
+import models
+import utils
+from fewshot_dataloader import get_fewshot_dataset, get_meta_loader, split_shot_query, make_nk_label
+
+def main(config):
+
+    #### Set the save and log path ####
+    svname = args.name
+    if svname is None:
+        svname = 'meta_{}-{}shot'.format(
+            config['train_dataset'], config['n_shot'])
+        svname += '_' + config['model'] + '-' + config['model_args']['encoder']
+    if args.tag is not None:
+        svname += '_' + args.tag
+    save_path = os.path.join('./save', svname)
+    utils.set_save_path(save_path)
+    utils.set_log_path(save_path)
+    writer = SummaryWriter(os.path.join(save_path, 'tensorboard'))
+    yaml.dump(config, open(os.path.join(save_path, 'meta_config.yaml'), 'w'))
+
+    #### Dataset ####
+    n_way, n_shot = config['n_way'], config['n_shot']
+    n_query = config['n_query']
+
+    #### Make datasets ####
+    # train
+    train_dataset = get_fewshot_dataset(config['dataset_path'], config['train_dataset'],
+                                        **config['train_dataset_args'])
+    train_dataloader = get_meta_loader(train_dataset, ways=n_way, shots=n_shot, query_shots=n_query
+                                       , batch_size=config['train_dataset_args']['batch_size'],num_workers=4)
+    utils.log('train dataset: {}'.format(config['train_dataset']))
+    if config.get('visualize_datasets'):
+        utils.visualize_dataset(train_dataset, 'train_dataset', writer)
+
+    # val
+    if config.get('val_dataset'):
+        val_dataset = get_fewshot_dataset(config['dataset_path'], config['val_dataset'],
+                                            **config['val_dataset_args'])
+        val_dataloader = get_meta_loader(val_dataset, ways=n_way, shots=n_shot, query_shots=n_query
+                                           , batch_size=config['val_dataset_args']['batch_size'], num_workers=4)
+        utils.log('val dataset: {}'.format(config['val_dataset']))
+        if config.get('visualize_datasets'):
+            utils.visualize_dataset(val_dataset, 'val_dataset', writer)
+    else:
+        val_dataloader = None
+
+    # test
+    if config.get('test_dataset'):
+        test_dataset = get_fewshot_dataset(config['dataset_path'], config['test_dataset'],
+                                          **config['test_dataset_args'])
+        test_dataloader = get_meta_loader(test_dataset, ways=n_way, shots=n_shot, query_shots=n_query
+                                         , batch_size=config['test_dataset_args']['batch_size'], num_workers=4)
+        utils.log('test dataset: {}'.format(config['test_dataset']))
+        if config.get('visualize_datasets'):
+            utils.visualize_dataset(test_dataset, 'test_dataset', writer)
+    else:
+        test_dataloader = None
+
+    #### Model and optimizer ####
+    if config.get('load'):
+        model_sv = torch.load(config['load'])
+        model = models.load(model_sv)
+    else:
+        model = models.make(config['model'], **config['model_args'])
+
+        if config.get('load_encoder'):
+            encoder = models.load(torch.load(config['load_encoder'])).encoder
+            model.online_encoder.load_state_dict(encoder.state_dict())
+
+    if config.get('_parallel'):
+        model = nn.DataParallel(model)
+
+    utils.log('num params: {}'.format(utils.compute_n_params(model)))
+
+    optimizer, lr_scheduler = utils.make_optimizer(
+            model.parameters(),
+            config['optimizer'], **config['optimizer_args'])
+
+    #### train and test ####
+    max_epoch = config['max_epoch']
+    save_epoch = config.get('save_epoch')
+    if save_epoch is None:
+        save_epoch = 5
+    max_va = 0.
+    timer_used = utils.Timer()
+    timer_epoch = utils.Timer()
+
+    # recode loss and accuracy
+    aves_keys = ['tl', 'ta', 'vl', 'va', 'tvl', 'tva']
+    trlog = dict()
+    for k in aves_keys:
+        trlog[k] = []
+
+    for epoch in range(1, max_epoch + 1):
+        timer_epoch.s()
+        aves = {k: utils.Averager() for k in aves_keys}
+
+        # train
+        model.train()
+        if config.get('freeze_bn'):
+            utils.freeze_bn(model) 
+        writer.add_scalar('lr', optimizer.param_groups[0]['lr'], epoch)
+
+        np.random.seed(epoch)
+        for data, _ in tqdm(train_dataloader, desc='train', leave=False):
+            data[0] = data[0].cuda()
+            data[1] = data[1].cuda()
+            data[2] = data[2].cuda()
+            x_shot_ori, x_query_ori = split_shot_query(
+                data[0], n_way, n_shot, n_query, ep_per_batch=config['train_dataset_args']['batch_size'])
+            x_shot_one, x_query_one = split_shot_query(
+                data[1], n_way, n_shot, n_query, ep_per_batch=config['train_dataset_args']['batch_size'])
+            x_shot_two, x_query_two = split_shot_query(
+                data[1], n_way, n_shot, n_query, ep_per_batch=config['train_dataset_args']['batch_size'])
+            label = make_nk_label(
+                n_way, n_query, ep_per_batch=config['train_dataset_args']['batch_size']).cuda()
+            # label = torch.cat([label1, label1], dim=0)
+
+            loss_byol, logits = model(x_shot_ori, x_query_ori, x_shot_one, x_query_one, x_shot_two, x_query_two)
+            logits = logits.view(-1, n_way)
+
+            loss_meta = F.cross_entropy(logits, label)
+
+            loss = loss_byol.mean() + loss_meta
+
+            acc = utils.compute_acc(logits, label)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            model.module.update_moving_average()
+            aves['tl'].add(loss.item())
+            aves['ta'].add(acc)
+
+        # eval
+        model.eval()
+        for name, loader, name_l, name_a in [
+                ('val', val_dataloader, 'vl', 'va'),
+                ('test', test_dataloader, 'tvl', 'tva')]:
+
+            if ((config.get('val_dataset') is None) and name == 'val') \
+                    or ((config.get('test_dataset') is None) and name == 'test'):
+                continue
+
+            np.random.seed(0)
+            for data, _ in tqdm(loader, desc=name, leave=False):
+                x_shot, x_query = split_shot_query(
+                    data.cuda(), n_way, n_shot, n_query, ep_per_batch=config['train_dataset_args']['batch_size'])
+
+                label = make_nk_label(
+                    n_way, n_query, ep_per_batch=config['train_dataset_args']['batch_size']).cuda()
+
+                with torch.no_grad():
+                    logits = model.module.test(x_shot, x_query).view(-1, n_way)
+                    loss_meta = F.cross_entropy(logits, label)
+                    acc = utils.compute_acc(logits, label)
+                
+                aves[name_l].add(loss_meta.item())
+                aves[name_a].add(acc)
+
+        _sig = int(_[-1])
+
+        # post
+        if lr_scheduler is not None:
+            lr_scheduler.step()
+
+        for k, v in aves.items():
+            aves[k] = v.item()
+            trlog[k].append(aves[k])
+
+        t_epoch = utils.time_str(timer_epoch.t())
+        t_used = utils.time_str(timer_used.t())
+        t_estimate = utils.time_str(timer_used.t() / epoch * max_epoch)
+        utils.log('epoch {}, train {:.4f}|{:.4f}, val {:.4f}|{:.4f}, '
+                'tval {:.4f}|{:.4f}, {} {}/{} (@{})'.format(
+                epoch, aves['tl'], aves['ta'], aves['vl'], aves['va'],
+                aves['tvl'], aves['tva'], t_epoch, t_used, t_estimate, _sig))
+
+        writer.add_scalars('loss', {
+            'train': aves['tl'],
+            'tval': aves['tvl'],
+            'val': aves['vl'],
+        }, epoch)
+        writer.add_scalars('acc', {
+            'train': aves['ta'],
+            'tval': aves['tva'],
+            'val': aves['va'],
+        }, epoch)
+
+        if config.get('_parallel'):
+            model_ = model.module
+        else:
+            model_ = model
+
+        training = {
+            'epoch': epoch,
+            'optimizer': config['optimizer'],
+            'optimizer_args': config['optimizer_args'],
+            'optimizer_sd': optimizer.state_dict(),
+        }
+        save_obj = {
+            'file': __file__,
+            'config': config,
+
+            'model': config['model'],
+            'model_args': config['model_args'],
+            'model_sd': model_.state_dict(),
+
+            'training': training,
+        }
+        torch.save(save_obj, os.path.join(save_path, 'epoch-last.pth'))
+        torch.save(trlog, os.path.join(save_path, 'trlog.pth'))
+
+        if (save_epoch is not None) and epoch % save_epoch == 0:
+            torch.save(save_obj,
+                    os.path.join(save_path, 'epoch-{}.pth'.format(epoch)))
+
+        if aves['va'] > max_va:
+            max_va = aves['va']
+            torch.save(save_obj, os.path.join(save_path, 'max-va.pth'))
+
+        writer.flush()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config')
+    parser.add_argument('--name', default=None)
+    parser.add_argument('--tag', default=None)
+    parser.add_argument('--gpu', default='0')
+    args = parser.parse_args()
+
+    config = yaml.load(open(args.config, 'r'), Loader=yaml.FullLoader)
+    if len(args.gpu.split(',')) > 1:
+        config['_parallel'] = True
+        config['_gpu'] = args.gpu
+
+    utils.set_gpu(args.gpu)
+    main(config)
+
